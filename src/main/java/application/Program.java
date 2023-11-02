44e900b66d88d173effd9536bed1c58b06878cac
Bruno <bruno.morales99@icloud.com>
2023-11-02 19:51:38 -0300

    Moving Pieces

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -3,9 +3,24 @@ package application;
 
 import chess.ChessMatch;
 
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        var sc = new Scanner(System.in);
         var chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            var source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            var target = UI.readChessPosition(sc);
+            
+            var capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
