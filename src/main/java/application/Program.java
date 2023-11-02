857b5501b93158eb0a2b1eeb85ed303b6e1441c6
Bruno <bruno.morales99@icloud.com>
2023-11-02 19:11:28 -0300

    Little improvement in board printing

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,8 +1,9 @@
-package org.application;
+package application;
+
 
 import chess.ChessMatch;
 
-public class Main {
+public class Program {
     public static void main(String[] args) {
         var chessMatch = new ChessMatch();
         UI.printBoard(chessMatch.getPieces());
