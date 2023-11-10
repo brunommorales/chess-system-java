8c8a2aadcae57f6fe5dac56281b67d9f88a969d2
Bruno <bruno.morales99@icloud.com>
2023-11-10 18:32:18 -0300

    Check logic

--- src/main/java/chess/Color.java
+++ src/main/java/chess/Color.java
@@ -1,6 +1,13 @@
 package chess;
 
+import java.util.List;
+import java.util.stream.Collector;
+
 public enum Color {
     BLACK,
     WHITE;
+
+    public Color collect(Collector<Object,?, List<Object>> list) {
+        return null;
+    }
 }
